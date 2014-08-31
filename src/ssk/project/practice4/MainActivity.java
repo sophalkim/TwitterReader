f66509c2e36a71120cb347cdb45ecae5faf4dbb1
Sophal Kim <sophal.kim.work@gmail.com>
2014-08-31 09:21:57 -0700

    Generating Documentation and Comments with my code

---
+++ src/ssk/project/practice4/MainActivity.java
@@ -0,0 +1,26 @@
+package ssk.project.practice4;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+
+	/*
+	 * Name of Twitter account that you are following.
+	 */
+	String screenName = "maroon5";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		downloadTweets();
+	}
+	
+	/*
+	 * Checks for Internet Connection, then Creates new DownloadTask for downloading
+	 * Tweets.
+	 */
+	public void downloadTweets() {
+		
+	}
+}
