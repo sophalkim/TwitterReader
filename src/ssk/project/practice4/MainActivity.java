eb88f878bc7723446c3bc51ffddb1b86fff54feb
Sophal Kim <sophal.kim.work@gmail.com>
2014-09-01 09:50:29 -0700

    Using "this" instead of creating a separate ListActivity object

--- src/ssk/project/practice4/MainActivity.java
+++ src/ssk/project/practice4/MainActivity.java
@@ -1,7 +1,12 @@
 package ssk.project.practice4;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends ListActivity {
 
@@ -21,6 +26,23 @@ public class MainActivity extends ListActivity {
 	 * Tweets.
 	 */
 	public void downloadTweets() {
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo info = connMgr.getActiveNetworkInfo();
+		if (info != null && info.isConnected()) {
+			new DownloadTwitterTask().execute(screenName);
+		} else {
+			Log.v("DownloadTweets()", "No Internet Connection");
+		}
+	}
+	
+	private class DownloadTwitterTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			return null;
+		}
 		
 	}
+	
+	
 }
